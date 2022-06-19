235084e54a49edd773dc00059fabd700ddf8319d
Mohd Mustajab Khan <meet2mustajabkhan@gmail.com>
2022-06-20 03:17:12 +0530

    Add files via upload

---
+++ PowerOfFour.java
@@ -0,0 +1,30 @@
+package GFG.Mathematical;
+
+public class PowerOfFour {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfFour(20));
+
+    }
+    static int isPowerOfFour ( long n)
+    {
+        // Your code here
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        else
+        {
+            if (n % 4 == 0)
+            {
+                return isPowerOfFour(n / 4);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
+
